7f1701e0e584e693af1379040de5a11ac6d5dfdb
Syed Hyder <syedhyderalihamdani@gmail.com>
2026-08-24 11:01:36 +0530

    Prepare Shortcut Vault for Raycast Store submission

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -15,14 +15,8 @@ declare type Preferences = ExtensionPreferences
 declare namespace Preferences {
   /** Preferences accessible in the `search-shortcuts` command */
   export type SearchShortcuts = ExtensionPreferences & {}
-  /** Preferences accessible in the `search-default-shortcuts` command */
-  export type SearchDefaultShortcuts = ExtensionPreferences & {}
-  /** Preferences accessible in the `search-custom-shortcuts` command */
-  export type SearchCustomShortcuts = ExtensionPreferences & {}
   /** Preferences accessible in the `add-shortcut` command */
   export type AddShortcut = ExtensionPreferences & {}
-  /** Preferences accessible in the `manage-custom-shortcuts` command */
-  export type ManageCustomShortcuts = ExtensionPreferences & {}
   /** Preferences accessible in the `export-shortcuts` command */
   export type ExportShortcuts = ExtensionPreferences & {}
   /** Preferences accessible in the `import-shortcuts` command */
@@ -32,14 +26,8 @@ declare namespace Preferences {
 declare namespace Arguments {
   /** Arguments passed to the `search-shortcuts` command */
   export type SearchShortcuts = {}
-  /** Arguments passed to the `search-default-shortcuts` command */
-  export type SearchDefaultShortcuts = {}
-  /** Arguments passed to the `search-custom-shortcuts` command */
-  export type SearchCustomShortcuts = {}
   /** Arguments passed to the `add-shortcut` command */
   export type AddShortcut = {}
-  /** Arguments passed to the `manage-custom-shortcuts` command */
-  export type ManageCustomShortcuts = {}
   /** Arguments passed to the `export-shortcuts` command */
   export type ExportShortcuts = {}
   /** Arguments passed to the `import-shortcuts` command */
